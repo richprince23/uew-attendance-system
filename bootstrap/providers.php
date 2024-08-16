4af8f61a9788da25c8fd76ddce332a3c1dfabdf9
= <=>
2024-08-16 04:00:18 +0000

    added student panel and savicon

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -4,4 +4,5 @@ return [
     App\Providers\AppServiceProvider::class,
     App\Providers\Filament\AdminPanelProvider::class,
     App\Providers\Filament\LecturerPanelProvider::class,
+    App\Providers\Filament\StudentPanelProvider::class,
 ];
