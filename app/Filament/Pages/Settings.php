ba373e4d3403f58689a214235314903f349986e7
= <=>
2024-08-19 01:43:23 +0000

    "Update navigation icon for Settings page from 'heroicon-o-document-text' to 'heroicon-o-cog-6-tooth'"

--- app/Filament/Pages/Settings.php
+++ app/Filament/Pages/Settings.php
@@ -6,7 +6,7 @@ use Filament\Pages\Page;
 
 class Settings extends Page
 {
-    protected static ?string $navigationIcon = 'heroicon-o-document-text';
+    protected static ?string $navigationIcon = 'heroicon-o-cog-6-tooth';
 
     protected static string $view = 'filament.pages.settings';
 }
